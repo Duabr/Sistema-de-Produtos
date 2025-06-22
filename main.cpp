a1cd7ed1d58e62b03fe5fc143a13f98adb9b10b6
Duabr <dauamfs04@gmail.com>
2025-06-22 15:14:22 -0300

    addCadastro e listarCadastros feitos
    Funções addCadastro e listarCadastros terminadas (eu acho), função rmvCadastro em andamento. Adicionados 5 produtos padrões no SI.

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@
 
 struct si
 {
-    std::vector<int> id = {1, 2, 3, 4, 5}; 
-    std::vector<std::string> nome = {};
-    std::vector<double> preco = {};
+    std::vector<int> id = {0, 1, 2, 3, 4};
+    std::vector<std::string> nome = {"Arroz 1KG", "Feijão 1KG", "Leite 1L", "Pipoca 500g", "Honda Civic 2010"};
+    std::vector<double> preco = {7.99, 5.99, 5.60, 4.99, 25900.0};
 } si; // Produtos do Sistema Interno (SI)
 
 struct carrinho
@@ -20,6 +20,7 @@ struct carrinho
     std::vector<double> preco;
     std::vector<int> quanti;
 }carrinho; // Produtos do Carrinho
+// Dica: dá pra usar a função "push_back" para adicionar um novo item ao final de uma lista (igual eu usei na função addCadastro por exemplo)
 
 int acharMaior(std::vector<int> lista){ // Achar o maior número de uma lista (para gerar um novo id pra todo novo cadastro no SI)
     int maior=lista[0];
@@ -49,13 +50,14 @@ int listaMenus(){ // Mostra os menus acessíveis com a opção de terminar o pro
 
 int menuCarrinho(){ // Mostra o menu do carrinho
     int escolha;
-    int n = 4;
+    int n = 5;
     printf("----------------------CARRINHO---------------------- \n");
     printf("0. Acessar lista de menus; \n");
     printf("1. Adicionar produto ao carrinho \n");
     printf("2. Remover produto do carrinho \n");
     printf("3. Atualizar um dos produtos do carrinho; \n");
     printf("4. Listar todos os produtos do carrinho; \n");
+    printf("5. Terminar compras (esvazia o carrinho e mostra o preço total)");
     printf("Digite um número de 0 a %d para escolher uma opção: ", n);
     scanf("%d", &escolha);
     while (escolha<0 or escolha>n){
@@ -87,31 +89,44 @@ int menuSI(){ // Mostra o menu do SI
     return escolha;
 }
 
-void addCadastro(std::string nome, int preco){ // Cadastra um produto no sistema
-    si.id.push_back(acharMaior(si.id));
-
-
-}
-
-
-
-void attCadastro(){ // Atualiza um produto do sistema
-
+void addCadastro(){ // Cadastra um produto no sistema
+    std::string nome;
+    double preco;
+    while ((getchar()) != '\n'); // É necessário para limpar o buffer e o std::getline() ler corretamente o nome do produto
+    std::cout << "Digite o nome do novo produto: ";
+    std::getline(std::cin, nome);
+    std::cout << "Digite o preço para o novo produto: ";
+    std::cin >> preco;
+    si.id.push_back(acharMaior(si.id)+1);
+    si.nome.push_back(nome);
+    si.preco.push_back(preco);
 }
 
 void rmvCadastro(){ // Remove um cadastro do sistema
+    int id;
+    int esc;
+    printf("Digite o ID do produto que deseja remover: ");
+    scanf("%d", &id);
+    std::cout << si.nome[id] << ", R$" << si.preco[id] << ". \n";
+    printf("É esse o produto que deseja remover? Digite 1 para sim: ");
+    scanf("%d", &esc);
+    if (esc==1){
+        printf("AHAM");
+    }
+}
+
+void attCadastro(){ // Edita um produto do sistema
 
 }
 
 void listarCadastros(){ // Lista todos os produtos cadastrados no sistema
-
+    for (int i=0; i<si.id.size(); i++){
+        std::cout << "Produto " << si.id[i] << ": " << si.nome[i] << ", R$" << si.preco[i] << std::endl;
+    }
+    system("pause");
 }
 
 void addProduCarrinho(){ // Adiciona um produto ao carrinho
-
-}
-
-void attProduCarrinho(){ // Atualiza a quantia de um dos produtos do carrinho
 
 }
 
@@ -119,11 +134,15 @@ void rmvProduCarrinho(){ // Remove um produto do carrinho
     
 }
 
+void attProduCarrinho(){ // Atualiza a quantia de um dos produtos do carrinho
+
+}
+
 void listarCarrinho(){ // Lista todos os produtos do carrinho
 
 }
 
-void limparCompras(){ // Finaliza as compras e esvazia o carrinho
+void limparCompras(){ // Finaliza as compras, esvazia o carrinho e retorna o usuário para a lista de menus
 
 }
 
@@ -134,7 +153,7 @@ int main() {
         escolha = listaMenus();
         if (escolha==1){ // Menu do carrinho
             while (true){
-                escolha=menuCarrinho();
+                escolha = menuCarrinho();
                 if (escolha==0){
                     break;
                 }
@@ -142,9 +161,15 @@ int main() {
         }
         if (escolha==2){ // Menu do SI
             while (true){
-                escolha=menuSI();
+                escolha = menuSI();
                 if (escolha==0){
                     break;
+                } else if (escolha==1){
+                    addCadastro();
+                } else if (escolha==2){
+                    rmvCadastro();
+                } else if (escolha==4){
+                    listarCadastros();
                 }
             }
         }
